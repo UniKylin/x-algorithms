e98cf51c83aba5f9acecc92dbaf2a28c52351d87
UniKylin <unikylin@126.com>
2017-08-10 01:34:45 +0800

    valid anagram

---
+++ 242-Valid Anagram/ant.js
@@ -0,0 +1,30 @@
+/**
+ *  Given two strings s and t, write a function to determine if t is an anagram of s.
+ *
+ *  For example,
+ *   s = "anagram", t = "nagaram", return true.
+ *   s = "rat", t = "car", return false.
+ * 
+ *  Note:
+ *  You may assume the string contains only lowercase alphabets.
+ */
+
+/**
+ * 排序后判断相等就可以了
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if (s.length != t.length) {
+    return false;
+  }  
+
+  var ant = s.split('').sort().join('');
+  var bear = t.split('').sort().join('');
+
+  return ant === bear;
+};
+
+console.log(isAnagram('anagram', 'nagaram'));
+console.log(isAnagram('rat', 'cat'));
