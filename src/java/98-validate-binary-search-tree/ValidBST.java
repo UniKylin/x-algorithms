85ab188e94bee74acf88b072d157e43ee68789aa
Kylin <unikylin@126.com>
2021-08-28 00:51:09 +0800

    validate binary search tree

---
+++ src/java/98-validate-binary-search-tree/ValidBST.java
@@ -0,0 +1,47 @@
+// link		: https://leetcode-cn.com/problems/validate-binary-search-tree/
+// Author	: Kylin
+// Date		: 2021-08-28
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            if (pre != null && root.val <= pre.val) {
+                return false;
+            }
+            pre = root;
+            root = root.right;
+        }
+
+        return true;
+
+    }
+}
