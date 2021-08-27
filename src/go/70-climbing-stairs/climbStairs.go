153ca9a119f5cdd8a507f0156f007c3fcb3fb0ea
Kylin <unikylin@126.com>
2021-08-28 00:20:44 +0800

    climb stairs

---
+++ src/go/70-climbing-stairs/climbStairs.go
@@ -0,0 +1,21 @@
+// link		: https://leetcode-cn.com/problems/climbing-stairs/
+// Author	: Kylin
+// Date		: 2021-08-28
+func climbStairs(n int) int {
+
+  if n <= 2 {
+    return n
+  }
+
+  var ant int = 1
+  var bear int = 2
+  var cat int = 3
+
+  for i := 3; i < n + 1; i++ {
+    cat = ant + bear
+    ant = bear
+    bear = cat
+  }
+
+  return cat
+}
