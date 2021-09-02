e01b194fc3d496c3469f584667c355df63de8a01
Kylin <unikylin@126.com>
2021-09-02 23:18:48 +0800

    move zeros

---
+++ src/go/283-move-zeroes/MoveZeros.go
@@ -0,0 +1,22 @@
+// link		: https://leetcode-cn.com/problems/move-zeroes/
+// Author	: Kylin
+// Date		: 2021-09-02
+
+func moveZeroes(nums []int) {
+	slow := 0
+	fast := 0
+
+	for ; fast < len(nums); fast++ {
+		if nums[fast] != 0 {
+			nums[slow] = nums[fast]
+			slow++
+		}
+
+		if fast == len(nums)-1 {
+			for slow != len(nums) {
+				nums[slow] = 0
+				slow++
+			}
+		}
+	}
+}
