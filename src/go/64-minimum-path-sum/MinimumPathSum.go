554ca466d73980f2128b227b4c154ff577f2e9df
Kylin <unikylin@126.com>
2021-09-02 22:42:01 +0800

    minimum path sum

---
+++ src/go/64-minimum-path-sum/MinimumPathSum.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func minPathSum(grid [][]int) int {
+	for row := 0; row < len(grid); row++ {
+		for column := 0; column < len(grid[0]); column++ {
+			if row == 0 && column == 0 {
+				continue
+			} else if row == 0 {
+				grid[row][column] = grid[row][column-1] + grid[row][column]
+			} else if column == 0 {
+				grid[row][column] = grid[row-1][column] + grid[row][column]
+			} else {
+				low := 0
+				if grid[row-1][column] <= grid[row][column-1] {
+					low = grid[row-1][column]
+				} else {
+					low = grid[row][column-1]
+				}
+				grid[row][column] = low + grid[row][column]
+			}
+		}
+	}
+
+	return grid[len(grid)-1][len(grid[0])-1]
+}
