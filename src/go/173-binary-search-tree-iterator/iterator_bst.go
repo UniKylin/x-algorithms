1d7ccb65867d5ce9fbac18f98e9461450ef504f9
Kylin <unikylin@126.com>
2022-01-04 23:51:01 +0800

    bst: iterator bst

--- src/go/173-binary-search-tree-iterator/iterator_bst.go
+++ src/go/173-binary-search-tree-iterator/iterator_bst.go
@@ -1,1 +1,40 @@
+// link		: https://leetcode-cn.com/problems/binary-search-tree-iterator/
+// Author	: Kylin
+// Date		: 2022-01-04
 
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type BSTIterator struct {
+	arr []int
+}
+
+func Constructor(root *TreeNode) (bst BSTIterator) {
+	bst.inorder(root)
+	return
+}
+
+func (bst *BSTIterator) inorder(node *TreeNode) {
+	if node == nil {
+		return
+	}
+
+	bst.inorder(node.Left)
+	bst.arr = append(bst.arr, node.Val)
+	bst.inorder(node.Right)
+}
+
+func (bst *BSTIterator) Next() int {
+	val := bst.arr[0]
+	bst.arr = bst.arr[1:]
+	return val
+}
+
+func (bst *BSTIterator) HasNext() bool {
+	return len(bst.arr) > 0
+}
---
+++ src/go/173-binary-search-tree-iterator/iterator_bst_test.go
@@ -0,0 +1,5 @@
+// link		: https://leetcode-cn.com/problems/binary-search-tree-iterator/
+// Author	: Kylin
+// Date		: 2022-01-04
+
+package leetcode
